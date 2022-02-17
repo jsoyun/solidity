f3e734ee1c308713a4bb650b49efbc891f7a90f9
jsoyun <syusmm233@gmail.com>
2022-02-17 16:54:05 +0900

    가나슈사용

--- ETHEREUM_WORKSPACE/deploy.js
+++ ETHEREUM_WORKSPACE/deploy.js
@@ -1,4 +1,5 @@
 const Web3 = require("web3");
+
 const fs = require("fs");
 
 const ABI = JSON.parse(
@@ -8,29 +9,39 @@ const BYTECODE = fs
   .readFileSync("./contracts_Votiing_test_sol_VotingTest.bin")
   .toString();
 
+const accounts = [];
 const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
-let accounts = web3.eth.getAccounts().then((data) => {
-  console.log(data);
-});
-//스마트 콘트랙트 전달
-const votingContract = new web3.eth.Contract(ABI, accounts[0]); //스마트콘트렉트
-//배포
-votingContract
-  .deploy({
-    data: BYTECODE,
-  })
-  .send({
-    from: accounts[0],
-    gas: 470000,
-    gasPrice: "80000000",
-  })
-  .then(function (newContractsInstance) {
-    console.log(newContractsInstance);
-  });
 
-votingContract.methods
-  .getVotedCount("yun")
-  .call()
-  .then((data) => {
-    console.log("Voted Count : ", data);
+async function init() {
+  await web3.eth.getAccounts().then((data) => {
+    data.forEach((x) => {
+      accounts.push(x);
+    });
   });
+}
+
+init().then(() => {
+  console.log(accounts);
+
+  //스마트 콘트랙트 전달
+  const votingContract = new web3.eth.Contract(ABI); //스마트콘트렉트
+  //배포
+  votingContract
+    .deploy({
+      data: BYTECODE,
+    })
+    .send({
+      from: accounts[1],
+      gas: 1562000,
+      gasPrice: "54300000000",
+    })
+    .then((newContractsInstance) => {
+      console.log(newContractsInstance.options.address);
+      newContractsInstance.methods
+        .getVotedCount("soyun")
+        .call()
+        .then((data) => {
+          console.log("Voted Count :" + data);
+        });
+    });
+});
