6910ffa4f033faa370c84ebe28f7b4a12ffa0fc9
jsoyun <syusmm233@gmail.com>
2022-03-10 13:11:32 +0900

    user,artist,music db연결

--- State_variable_project/models/user.js
+++ State_variable_project/models/user.js
@@ -1,41 +1,50 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('user', {
-    id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING(45),
-      allowNull: false
-    },
-    nation: {
-      type: DataTypes.STRING(45),
-      allowNull: false
-    },
-    favor_genre: {
-      type: DataTypes.STRING(45),
-      allowNull: true
-    },
-    address: {
-      type: DataTypes.STRING(45),
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    tableName: 'user',
-    timestamps: false,
-    indexes: [
+const Sequelize = require("sequelize");
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
       {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
+        id: {
+          autoIncrement: true,
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+        },
+        name: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+        },
+        nation: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+        },
+        favor_genre: {
+          type: Sequelize.STRING(45),
+          allowNull: true,
+        },
+        address: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+        },
       },
-    ]
-  });
+      {
+        sequelize,
+        tableName: "user",
+        timestamps: false,
+        indexes: [
+          {
+            name: "PRIMARY",
+            unique: true,
+            using: "BTREE",
+            fields: [{ name: "id" }],
+          },
+        ],
+      }
+    );
+  }
+  static associate(db) {
+    db.User.hasMany(db.Artist, {
+      foreignKey: "user_idUser",
+      sourceKey: "artists",
+    });
+  }
 };
